08f626bbfedd1992f9140c2222acc2bd4610f6fa
ikhana <Inaa.eth@gmail.com>
2024-02-20 12:25:50 +0500

    typeCasting

---
+++ typeCasting.go
@@ -0,0 +1,18 @@
+// Hello World Start from here
+package main
+
+import (
+	"fmt"
+)
+
+var pl = fmt.Println
+
+func main() {
+
+	var v1 float64 = 1.2
+
+	var v2 = int(v1)
+
+	pl(v2) // Will print 1
+
+}
