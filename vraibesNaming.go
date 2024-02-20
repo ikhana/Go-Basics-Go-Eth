c5f8b90c3e7fb3571259e286a64bd8cb0195024e
ikhana <Inaa.eth@gmail.com>
2024-02-20 12:09:14 +0500

    namingConvetions

---
+++ vraibesNaming.go
@@ -0,0 +1,16 @@
+// Hello World Start from here
+package main
+
+func variableConvetion() {
+
+	// varaible name followed by type of that varaible
+	// camelCase varibles are convetion
+	// if a variabl has been declared in CAPITALS then it can be accessed outside of the package
+	// varaibles are mutable as long as the data type is the same
+	var vName string = "Inaam"
+
+	var v1, v2 = 2.3, 3.3 // here we are telling the compiler to go and figure out the type for us
+
+	var v3 = "hello"
+
+}
