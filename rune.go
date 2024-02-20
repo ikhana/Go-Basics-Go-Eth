c30100862ac162d84755e125991ea491853e7a8b
ikhana <Inaa.eth@gmail.com>
2024-02-20 13:40:39 +0500

    rune and time

--- rune.go
+++ rune.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 	"unicode/utf8"
 )
 
@@ -18,5 +19,7 @@ func main() {
 	for i, runeValue := range vRune {
 		fmt.Printf("%d : %#U : %c\n ", i, runeValue, runeValue)
 	}
+	now := time.Now()
+	pl(now.Year(), now.Month(), now.Day(), now.Hour(), now.Minute(), now.Second()) // As of writng and runing this code it has orinted 2024 February 20 13 39 48
 
 }
