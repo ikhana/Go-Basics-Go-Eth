560167bf600a3bfebf391da6a1395af1db9cb66a
ikhana <Inaa.eth@gmail.com>
2024-02-22 15:10:58 +0500

    passing refrence address

--- pointers.go
+++ pointers.go
@@ -23,9 +23,13 @@ func main() {
 	passByValue := 4
 	passByRefrence := 4
 
-	fmt.Println("Values before of pass by value and pass by refrence respectivel :\n", passByValue, "\n", passByRefrence)
+	/// We can store the pointer addres as well
+
+	var pointerAddress *int = &passByRefrence
+
+	fmt.Println("Values before of pass by value and pass by refrence and address of the pointer at memory leve  respectivel :\npassByValye\n", passByValue, "\npassByRefrence\n", passByRefrence, "\nAddress\n", pointerAddress)
 
 	fmt.Println(chnageValue(passByValue, &passByRefrence))
 
-	fmt.Println("Values after of pass by value and pass by refrence respectivel :\n", passByValue, "\n", passByRefrence)
+	fmt.Println("Values after of pass by value and pass by refrence and address of the pointer at memory leve  respectivel :\npassByValye\n", passByValue, "\npassByRefrence\n", passByRefrence, "\nAddress\n", pointerAddress)
 }
